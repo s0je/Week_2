d68f6d63b7f789697aff93f61723cd809d2c2b79
s0je <97879081+s0je@users.noreply.github.com>
2022-01-22 17:02:22 +0100

    Delete personTest.js

--- Typescript/personTest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var person_1 = require("./person");
-var persona1 = new person_1.Person("Pepito", 30, "Calle Mayor");
-persona1.printName();
-console.log(persona1.yearOfBirth(2022));
-persona1.setAddress("Calle Principal");
-console.log(persona1.getAddress());
